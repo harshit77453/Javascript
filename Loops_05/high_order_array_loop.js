591b991791a243bf8707bf931da973c9ad5667b0
Harshit Chauhan <140307979+shit-an@users.noreply.github.com>
2024-08-07 17:56:12 +0000

    commit

--- Loops_05/high_order_array_loop.js
+++ Loops_05/high_order_array_loop.js
@@ -23,14 +23,36 @@ for (const [key1,value] of map) {
 //         Fr:-France
 
 // forof loop Not work in object.
-const obj = {
-    "1":"India",
-    "2":"USA",
-    "3":"France"
-} 
-for (const [key,value] of obj) {
-    console.log(key,value)
+// const obj = {
+//     "1":"India",
+//     "2":"USA",
+//     "3":"France"
+// } 
+// for (const [key,value] of obj) {
+//     console.log(key,value)
 
-// give Error - TypeError: obj is not iterable
+// // give Error - TypeError: obj is not iterable
     
+// }
+
+
+// for in Loop- objects can be iterable using for in Loop.
+
+const myobject = {
+    "js":"Javascript",
+    "C++": "C Plus Plus",
+    "py": "Python"
 }
+for (const key in myobject) {
+    console.log(`${key} shortcut of ${myobject[key]}`)
+} 
+
+// Note- arrays also have key, index value in array is referred as key
+//     - objects is designed to set the index as key, as keys are sets by users so it solve the fixed key value problem as index in array 
+
+const programming = ["js","Python","C++","C"]
+for (const key in programming) {
+    console.log(key)                // It output as index value from 0 to 3
+    console.log(programming[key])
+}                                   // It output as value from 0 to 3 index.
+
